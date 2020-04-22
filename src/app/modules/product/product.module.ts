689f9f90edac1438d02b986f9a4b24db4f65b538
dpedra5 <dpedra5@bancodebogota.com.co>
2020-04-22 18:44:28 -0500

    Add angular material

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -5,6 +5,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
 import { ProductsComponent } from './components/products/products.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { SharedModule } from '../../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    MatButtonModule
   ]
 })
 export class ProductModule {
